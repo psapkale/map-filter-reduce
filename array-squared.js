56c6d113033c325d1d9b8dc2788fe558f2ca9779
Prem <prempravinsapkale@gmail.com>
2024-05-14 12:42:35 +0530

    init

---
+++ array-squared.js
@@ -0,0 +1,7 @@
+// Square the value of every element in the array. Presume that you will only get numbers in the input array.
+
+const arr = [5, 3, 1, 2, 6];
+
+const output = arr.map((x) => Math.pow(x, 2));
+
+console.log(output);
